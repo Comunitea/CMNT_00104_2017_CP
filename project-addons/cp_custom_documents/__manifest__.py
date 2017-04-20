7fa5d0deede41271f83b0fac4069688828253f8a
jesusVMayor <jesus@comunitea.com>
2017-04-20 17:15:29 +0200

    [IMP]cp_custom_documents: Añadidas modificaciones sobre informe de ventas y factura. Añadido informe de exención de iva.

---
+++ project-addons/cp_custom_documents/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Custom documents',
+    'summary': '',
+    'version': '10.0.1.0.0',
+    'category': 'Uncategorized',
+    'website': 'comunitea.com',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'base',
+        'account',
+        'port_scale',
+        'port_scale_sale'
+    ],
+    'data': [
+        'views/report_saleorder.xml',
+        'views/report_invoice.xml',
+        'views/iva_report.xml'
+    ],
+}
