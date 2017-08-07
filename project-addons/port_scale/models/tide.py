e4cb1a4535c396f94a1aaa45d8396da2245ec724
jesusVMayor <jesus@comunitea.com>
2017-08-07 17:15:53 +0200

    [IMP]port_scale: Se importa la altura de las mareas.

--- project-addons/port_scale/models/tide.py
+++ project-addons/port_scale/models/tide.py
@@ -13,17 +13,27 @@ class PortTide(models.Model):
 
     date = fields.Date()
     bajamar_1 = fields.Char()
+    altura_bajamar_1 = fields.Char()
     bajamar_2 = fields.Char()
+    altura_bajamar_2 = fields.Char()
     pleamar_1 = fields.Char()
+    altura_pleamar_1 = fields.Char()
     pleamar_2 = fields.Char()
+    altura_pleamar_2 = fields.Char()
 
     @api.model
     def get_today_tides(self):
         tide = self.search([('date', '=', date.today())])
         if not tide:
-            return [('Bajamar', '-', '-'), ('Pleamar', '-', '-')]
-        return [('Bajamar', tide[0].bajamar_1, tide[0].bajamar_2),
-                ('Pleamar', tide[0].pleamar_1, tide[0].pleamar_2)]
+            return [('Bajamar', '-(-)', '-(-)'), ('Pleamar', '-(-)', '-(-)')]
+        return [('Bajamar', '%s (%s)' %
+                 (tide[0].bajamar_1 or '', tide[0].altura_bajamar_1 or ''),
+                 '%s (%s)' % (tide[0].bajamar_2 or '',
+                              tide[0].altura_bajamar_2 or '')),
+                ('Pleamar', '%s (%s)' %
+                 (tide[0].pleamar_1 or '', tide[0].altura_pleamar_1 or ''),
+                 '%s (%s)' % (tide[0].pleamar_2 or '',
+                              tide[0].altura_pleamar_2 or ''))]
 
     @api.model
     def import_feed_rss_tides(self):
@@ -33,9 +43,13 @@ class PortTide(models.Model):
         vals = {
             'date': date.today(),
             'bajamar_1': tide_list['Bajamar'][0][0],
+            'altura_bajamar_1': tide_list['Bajamar'][0][1],
             'bajamar_2': tide_list['Bajamar'][1][0],
+            'altura_bajamar_2': tide_list['Bajamar'][1][1],
             'pleamar_1': tide_list['Pleamar'][0][0],
+            'altura_pleamar_1': tide_list['Pleamar'][0][1],
             'pleamar_2': tide_list['Pleamar'][1][0],
+            'altura_pleamar_2': tide_list['Pleamar'][1][1],
         }
         exist_tide = self.env['port.tide'].search(
             [('date', '=', date.today())])
