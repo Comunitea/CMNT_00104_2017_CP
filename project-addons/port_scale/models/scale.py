3c7f8a622f0436a35542eb0ad864e4f55f81953f
jesusVMayor <jesus@comunitea.com>
2017-10-04 12:17:38 +0200

    [FIX]port_scale: GT editable.

--- project-addons/port_scale/models/scale.py
+++ project-addons/port_scale/models/scale.py
@@ -20,7 +20,7 @@ class PortScale(models.Model):
     name = fields.Char('Nº escala')
     active = fields.Boolean(default=True)
     ship = fields.Many2one('ship', required=True)
-    gt = fields.Integer("GT", related="ship.gt", readonly=True)
+    gt = fields.Integer("GT", related="ship.gt")
     origin = fields.Char()
     operation = fields.Selection(
         (('D', 'disembarkation'),
