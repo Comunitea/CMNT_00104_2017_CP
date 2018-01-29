4bb043bc81632fcf526ba411bcf38264f06192f7
Alberto Luengo Cabanillas <alberto@luengocabanillas.es>
2018-01-29 23:40:41 +0100

    [ADD][aluengo][port_scale] Se añade un cron para eliminar el historial de entradas de la importación del día anterior

--- project-addons/port_scale/models/port_scale_history.py
+++ project-addons/port_scale/models/port_scale_history.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api, fields
+from datetime import date, timedelta
 
 
 class PortScaleHistory(models.Model):
@@ -11,6 +12,19 @@ class PortScaleHistory(models.Model):
     """
     _name = 'port.scale.history'
     _order = 'date_execution desc'
+
+    @api.model
+    def cron_delete_yesterday_info(self):
+        """
+        Borramos el historico de las importaciones del día anterior
+        """
+        yesterday = date.today() - timedelta(1)
+        first_hour_yesterday = yesterday.strftime('%Y-%m-%d 00:00:01')
+        last_hour_yesterday = yesterday.strftime('%Y-%m-%d 23:59:59')
+        yesterday_scales_history = self.search([('date_execution','>=',first_hour_yesterday),('date_execution','<=',last_hour_yesterday)])
+        if yesterday_scales_history:
+            yesterday_scales_history.unlink()
+
 
     date_execution = fields.Datetime(string='Fecha Ejecución', required=True)
     scale_id = fields.Many2one('port.scale', string='Escala',required=True)
